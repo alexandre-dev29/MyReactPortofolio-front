c3c29f6e2439f7c439126cf8523b8c64dfe72d2d
Alexandre <axel.business29@gmail.com>
2019-03-05 22:49:54 +0200

    Adding react-pose for anmation javascript , adding Allroutes Component for defining all routes

---
+++ src/Components/AllRoutes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {  Route,  Switch, withRouter } from 'react-router-dom';
+import posed, { PoseGroup } from 'react-pose';
+import Home from './Pages/Home/Home';
+import Skills from './Pages/Skills/AlllSkills';
+import Works from './Pages/Works/AllWorks';
+import Resume from './Pages/Resumes/Resume';
+import Contacts from './Pages/Contacts/ContactsPage';
+
+const RouteContainer = posed.div({
+  enter: { opacity: 1, delay: 300, beforeChildren: true },
+  exit: { opacity: 0 }
+});
+
+const AllRoutes = () => (
+    <Route
+        render={({ location }) => (
+        <div>
+            <div>
+            <PoseGroup>
+                <RouteContainer key={location.key}>
+                <Switch location={location}>
+                    <Route exact path="/" component={Home} key="Home" />
+                    <Route path="/Skills" component={Skills} key="Skills" />
+                    <Route path="/Works" component={Works} key="Works" />
+                    <Route path="/Resume" component={Resume} key="Resume" />
+                    <Route path="/Contacts" component={Contacts} key="Contacts" />
+                </Switch>
+                </RouteContainer>
+            </PoseGroup>
+            </div>
+        </div>
+        )}
+    />
+);
+export default withRouter(AllRoutes);
