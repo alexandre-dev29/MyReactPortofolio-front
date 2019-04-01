f07323f3285f1d582a979564244f99f503151a0a
Alexandre <axel.business29@gmail.com>
2019-04-01 15:31:52 +0200

    adding read routes adding article adding http call for populate articles refractor some codes

--- src/Components/Pages/ReadMe/Article.jsx
+++ src/Components/Pages/ReadMe/Article.jsx
@@ -2,36 +2,28 @@ import React, {Component} from 'react';
 
 class Article extends Component {
     render() {
+        const {title, link, description, image} =this.props;
         return (
             <div className="col-4">
                 <div className="card">
 
                     <div className="view overlay">
                         <img className="card-img-top"
-                             src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%28131%29.jpg"
+                             src={process.env.REACT_APP_API_URL+ "/" + image}
                              alt="Card image cap"/>
                         <a>
                             <div className="mask rgba-white-slight"></div>
                         </a>
                     </div>
 
-
                     <div className="card-body">
-
-
-                        <h4 className="card-title text-center text-success h3">Card title</h4>
+                        <h4 className="card-title text-center text-success h4">{title}</h4>
                         <hr/>
-
-                        <p className="card-text">Some quick example text to build on the card title
-                            and make up the bulk of the card's content.</p>
-
-                        <a href="#!" target="_blank" className="black-text d-flex justify-content-end"><h5>Read
+                        <p className="card-text">{description}</p>
+                        <a href={link} target="_blank" className="black-text d-flex justify-content-end"><h5>Read
                             more <i className="fas fa-angle-double-right"></i></h5></a>
-
                     </div>
-
                 </div>
-
             </div>
         );
     }
--- src/Components/Pages/ReadMe/Readme.jsx
+++ src/Components/Pages/ReadMe/Readme.jsx
@@ -1,19 +1,58 @@
 import React, { Component } from 'react'
-
-import { Row, Col, Button } from "reactstrap";
-import Skill from "../Skills/Skill";
-import Works from "../Works/work";
 import Article from "./Article";
+import {ApiCall} from "../../../Utils/ApiConfig";
+
+const articlepath = process.env.REACT_APP_API_URL + "/articles";
 
 export default class Readme extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            all_articles: [
+               /* {
+                    title: "Remote Jobs Is The Future For Developers",
+                    image: "../../assets/img/project.jpg",
+                    description: "Cet article vous parle des differents avantage du travail en remote pour les entreprise et les developers",
+                    link: "https://medium.com"
+                },
+                {
+                    title: "Remote Jobs Is The Future For Developers",
+                    image: "../../assets/img/project.jpg",
+                    description: "Cet article vous parle des differents avantage du travail en remote pour les entreprise et les developers",
+                    link: "https://medium.com"
+                },
+                {
+                    title: "Remote Jobs Is The Future For Developers",
+                    image: "../../assets/img/project.jpg",
+                    description: "Cet article vous parle des differents avantage du travail en remote pour les entreprise et les developers",
+                    link: "https://medium.com"
+                },*/
+            ],
+            isLoading: false
+        }
+    }
 
-  componentDidMount() {
+
+    componentDidMount() {
     document.body.classList.toggle("landing-page");
+
+        ApiCall.get(articlepath+"/get_articles", {
+            params : {
+                "user_email": process.env.REACT_APP_EMAIL_CONFIG,
+            }
+        }).then(res => {
+            console.log(res);
+            this.setState({
+                all_articles: res.data.all_data,
+                isLoading: true
+            })
+        });
   }
   componentWillUnmount() {
     document.body.classList.toggle("landing-page");
   }
   render() {
+        const  {all_articles, isLoading} =this.state;
     return (
       <div>
         <div className="page-header">
@@ -54,12 +93,22 @@ export default class Readme extends Component {
                             <h1 className="text-info text-center mt-lg display-3">
                                 Articles
                             </h1>
-                            <div className="row justify-content-start">
-                               <Article/>
-                               <Article/>
-                               <Article/>
-                            </div>
-
+                                {!isLoading ?
+                                    <div className="row justify-content-center">
+                                        <img src={require("../../../assets/img/Loading/loading_infinity.svg")} alt="loading works" />
+                                    </div> :
+                                    <div className="row justify-content-start">
+                                        {all_articles.map((article, index) => (
+                                            <Article
+                                                key={article._id}
+                                                title={article.article_title}
+                                                image={article.article_image}
+                                                link={article.article_link}
+                                                description={article.article_description}
+                                            />
+                                        ))}
+                                    </div>
+                                }
                         </div>
                     </div>
                 </div>
